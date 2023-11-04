79d53c6fb3c93080d77c24e90cd03c1f93e602c2
Noah Dobie <NoahRDobie@gmail.com>
2023-11-04 15:18:59 -0700

    Added Pie Chart in statistics screen. 2 Functions to call: - Setup (only needed on startup) - Set Data (can be modified to be used each time new data is created)

--- app/src/main/java/com/example/basketballshottracker/ui/statistics/StatsFragment.java
+++ app/src/main/java/com/example/basketballshottracker/ui/statistics/StatsFragment.java
@@ -1,15 +1,25 @@
 package com.example.basketballshottracker.ui.statistics;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.basketballshottracker.R;
 import com.example.basketballshottracker.databinding.FragmentStatsBinding;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+
+import java.util.ArrayList;
 
 public class StatsFragment extends androidx.fragment.app.Fragment {
 
@@ -23,9 +33,51 @@ public class StatsFragment extends androidx.fragment.app.Fragment {
         binding = FragmentStatsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        viewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //Create chart for statistics screen
+        PieChart statsChart = (PieChart) root.findViewById(R.id.statsChart);
+        setupChart(statsChart);
+        setChartData(statsChart);
+
         return root;
+    }
+
+    //Just needed to setup the chart, once setup does not need to be called again
+    private void setupChart(PieChart chart) {
+        chart.setDrawHoleEnabled(true);
+        chart.setUsePercentValues(true);
+        chart.setEntryLabelTextSize(16);
+        chart.setEntryLabelColor(Color.BLACK);
+        chart.setCenterText("Accuracy");
+        chart.setCenterTextSize(24);
+        chart.setCenterTextColor(Color.BLACK);
+        chart.getDescription().setEnabled(false);
+    }
+    //Can be modified to be public
+    //Then we can create a ArrayList that stores the current session info
+    //To then update this dynamically as the user progresses
+    //Then we store all previous data in Arraylists as well
+    private void setChartData(PieChart chart) {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(60, "Made"));
+        entries.add(new PieEntry(40, "Missed"));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(ContextCompat.getColor(getContext(), R.color.accent));
+        colors.add(ContextCompat.getColor(getContext(), R.color.darkSecondary));
+
+        PieDataSet dataSet = new PieDataSet(entries, "Accuracy");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true);
+        data.setValueFormatter(new PercentFormatter(chart));
+        data.setValueTextSize(22f);
+        data.setValueTextColor(Color.BLACK);
+
+        chart.setData(data);
+        chart.invalidate();
+
+        chart.animateY(1000, Easing.EaseInOutQuad);
     }
 
     @Override
