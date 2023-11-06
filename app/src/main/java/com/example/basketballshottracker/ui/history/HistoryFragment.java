ace01ed19d2f5db634481588face65272c3b30d6
JacobH <jakey707@hotmail.com>
2023-11-05 22:50:13 -0800

    Can now read data from a dictionary hopefully.

--- app/src/main/java/com/example/basketballshottracker/ui/history/HistoryFragment.java
+++ app/src/main/java/com/example/basketballshottracker/ui/history/HistoryFragment.java
@@ -18,7 +18,11 @@ import com.example.basketballshottracker.R;
 import com.example.basketballshottracker.databinding.FragmentHistoryBinding;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
@@ -31,6 +35,8 @@ public class HistoryFragment extends Fragment {
 
     private FragmentHistoryBinding binding;
 
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HistoryViewModel historyViewModel =
@@ -41,8 +47,16 @@ public class HistoryFragment extends Fragment {
 
         Context thiscontext = container.getContext();
 
-        createGroupList();
-        createCollection();
+        // Creating a dictionary to hold dummy data
+        // contains key value pairs where the key is the date and the integer array contains the values
+        // the values will be in the order of shots made, shots missed, and shots taken
+        Dictionary<String, int[]> dict = new Hashtable<>();
+        dict.put("November 1st", new int[]{13, 14, 14});
+        dict.put("November 3rd", new int[]{5, 10, 20});
+        dict.put("November 4th", new int[]{1, 2, 3});
+
+        createGroupList(dict);
+        createCollection(dict);
         expandableListView = root.findViewById(R.id.listStats);
         expandableListAdapter = new MyExpandableListAdapter(thiscontext, groupList, mCollection);
         expandableListView.setAdapter(expandableListAdapter);
@@ -73,21 +87,30 @@ public class HistoryFragment extends Fragment {
 
     }
 
-    private void createCollection() {
-        String [] session1stat = {"60"};
-        String [] session2stat = {"40"};
+    // Hopefully we can read from a dictionary now
+    // Successfully grabs from dictionary and gets correct string data
+    // Not in order though hmm
+    private void createCollection(Dictionary dict) {
         mCollection = new HashMap<String, List<String>>();
-        for (String group : groupList) {
-            if (group.equals("Session 1")) {
-                loadChild(session1stat);
+        Enumeration<String> keys = dict.keys();
+        while (keys.hasMoreElements()){
+            for (String group : groupList) {
+                String key = keys.nextElement();
+                if (group.equals(key))
+                    loadChild(Arrays.toString((int[]) dict.get(key)).split("[\\[\\]]")[1].split(", "));
+                //if (group.equals("Session 1")) {
+                //    loadChild(session1stat);
+                mCollection.put(group, childList);
             }
-            else if (group.equals("Session 2")) {
-                loadChild(session2stat);
-            }
-            mCollection.put(group, childList);
+            //else if (group.equals("Session 2")) {
+            //    loadChild(session2stat);
+            //}
+
         }
+
     }
 
+    // Find a way to label where the data is from??
     private void loadChild(String[] sessionstats) {
         childList = new ArrayList<>();
         for (String model : sessionstats) {
@@ -95,10 +118,13 @@ public class HistoryFragment extends Fragment {
         }
     }
 
-    private void createGroupList() {
+    // Losing my mind
+    private void createGroupList(Dictionary dict) {
         groupList = new ArrayList<>();
-        groupList.add("Session 1");
-        groupList.add("Session 2");
+        Enumeration<String> keys = dict.keys();
+        while (keys.hasMoreElements()) {
+            groupList.add(keys.nextElement());
+        }
     }
 
     @Override
